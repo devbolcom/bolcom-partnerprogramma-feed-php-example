342fa4f984e0b98c5e954319872e3d50848aad72
Derk Braakman <derkmdt@hotmail.com>
2018-03-13 21:45:07 +0100

    Fixed failing Unit test

--- Tests/LoadXmlTest.php
+++ Tests/LoadXmlTest.php
@@ -8,8 +8,8 @@ class LoadXmlTest extends TestCase
     public function testLoadXml()
     {
         printXml();
-        $this->expectOutputString('ProductId: 1004004000008454 - Title: De beer is los - Prijs: 10.9
-ProductId: 1004004006510342 - Title: Ticket to Ride Europe - Bordspel - Prijs: 37.95
+        $this->expectOutputString('ProductId: 1004004000008454 - Title: De beer is los - Price: 10.9
+ProductId: 1004004006510342 - Title: Ticket to Ride Europe - Bordspel - Price: 37.95
 ');
     }
 
